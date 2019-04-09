3abd23b8c0f0ce953bfd0e07b4e7e08878b2e1c7
alvaromanrique55 <49073596+alvaromanrique55@users.noreply.github.com>
2019-04-09 20:34:48 +0200

    Add files via upload

---
+++ github.java
@@ -0,0 +1,45 @@
+package programacion;
+
+public class github {
+	
+	 public static void main(String[]args){
+	        int[] numeros = new int[10];
+	        int pos = 0, neg = 0, cero = 0; 
+	        int i;
+	       
+	
+	        System.out.println("Lectura de los elementos del array: ");
+	        for (i = 0; i < numeros.length; i++) {
+	            System.out.print("numeros[" + i + "]= ");
+	            
+			
+	        }
+	       
+	        for (i = 0; i < numeros.length; i++) {
+	            if (numeros[i] > 0) {
+	                pos++;
+	            } else if (numeros[i] < 0) {
+	                neg++;
+	            } else {
+	                cero++;
+	            }
+	        }
+	     
+	        System.out.println("Positivos: " + pos);
+	        System.out.println("Negativos: " + neg);
+	        System.out.println("Ceros: " + cero);
+	        
+	        System.out.println("este es el cambio ");
+	        
+	        
+	    }
+}
+
+
+	
+	
+	
+	
+	
+
+
